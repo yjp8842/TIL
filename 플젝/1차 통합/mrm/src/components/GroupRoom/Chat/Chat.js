56b6117d0b564fbf0528d0b6a3ce1b36ef2327eb
yjp8842 <yjp20183029@gmail.com>
2023-02-06 22:00:35 +0900

    0206: Chat.js modify

--- 플젝/1차 통합/mrm/src/components/GroupRoom/Chat/Chat.js
+++ 플젝/1차 통합/mrm/src/components/GroupRoom/Chat/Chat.js
@@ -1,9 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import axios from "axios";
-import SockJS from "sockjs-client";
-import { Stomp } from "@stomp/stompjs";
+// import SockJS from "sockjs-client";
+// import { Stomp } from "@stomp/stompjs";
+import * as StompJs from "@stomp/stompjs";
+import * as SockJS from "sockjs-client";
+
+const ROOM_SEQ = 1;
 
 const Chat = () => {
+  const client = useRef({});
   let [roomId, setRoomId] = useState();
   let [room, setRoom] = useState({});
   let [userId, setUserId] = useState("");
@@ -13,25 +18,87 @@ const Chat = () => {
   let [time, setTime] = useState("");
   // let [sock, setSock] = useState();
   // let [ws, setWs] = useState();
-  let [reconnect, setReconnect] = useState(0);
+  // let [reconnect, setReconnect] = useState(0);
 
   const BASE_URL = 'https://i8a406.p.ssafy.io/api';
   
-  var sock = new SockJS(BASE_URL+"/ws/chat");
-  var ws = Stomp.over(function(){
-    return new SockJS(BASE_URL+"/ws/chat");
-  });
-
+  // var sock = new SockJS(BASE_URL+"/ws/chat");
+  // var ws = Stomp.over(function(){
+  //   return new SockJS(BASE_URL+"/ws/chat");
+  // });
 
   useEffect(() => {
-    setRoomId(parseInt(localStorage.getItem("wschat.roomId")));
-    setUserId(localStorage.getItem("wschat.userId"));
-    setUserNickname(localStorage.getItem("wschat.userNickname"));
+    connect();
+    initRoom();
 
-    console.log(parseInt(localStorage.getItem("wschat.roomId")).type);
-    // connect();
-    // initRoom();
+    return () => disconnect();
   }, []);
+
+  const connect = () => {
+    client.current = new StompJs.Client({
+      // brokerURL: "ws://localhost:8080/ws-stomp/websocket", // 웹소켓 서버로 직접 접속
+      webSocketFactory: () => new SockJS(BASE_URL+"/ws/chat"), // proxy를 통한 접속
+      connectHeaders: {
+        "auth-token": "spring-chat-auth-token",
+      },
+      debug: function (str) {
+        console.log(str);
+      },
+      reconnectDelay: 5000,
+      heartbeatIncoming: 4000,
+      heartbeatOutgoing: 4000,
+      onConnect: () => {
+        subscribe();
+      },
+      onStompError: (frame) => {
+        console.error(frame);
+      },
+    });
+
+    client.current.activate();
+  };
+
+  const disconnect = () => {
+    client.current.deactivate();
+  };
+
+  const subscribe = () => {
+    client.current.subscribe(BASE_URL+`/topic/chat/room/${ROOM_SEQ}`, (message) => {
+      var recv = JSON.parse(message.body);
+      recvMessage(recv);
+      // setMessages((_chatMessages) => [..._chatMessages, JSON.parse(body)]);
+    });
+  };
+
+  const publish = (message) => {
+    if (!client.current.connected) {
+      return;
+    }
+
+    client.current.publish({
+      destination: BASE_URL+"/app/chat/message",
+      body: JSON.stringify({
+        // type: "TALK",
+        roomId: parseInt(roomId),
+        userId: userId,
+        userNickname: userNickname,
+        message: message,
+        time: "오늘",
+      }),
+    });
+
+    setMessage("");
+  };
+
+  // useEffect(() => {
+  //   setRoomId(parseInt(localStorage.getItem("wschat.roomId")));
+  //   setUserId(localStorage.getItem("wschat.userId"));
+  //   setUserNickname(localStorage.getItem("wschat.userNickname"));
+
+  //   console.log(parseInt(localStorage.getItem("wschat.roomId")).type);
+  //   // connect();
+  //   // initRoom();
+  // }, []);
 
   const initRoom = () => {
     axios.get(BASE_URL+`/chat/room/${roomId}`).then(response => {
@@ -51,26 +118,26 @@ const Chat = () => {
     });
   };
 
-  const sendMessage = (message) => {
-    console.log("send 1");
-    console.log(roomId);
-    let url = BASE_URL+"/app/chat/message";
-    console.log(url);
-    ws.send(
-      url,
-      {},
-      JSON.stringify({
-        // type: "TALK",
-        roomId: parseInt(roomId),
-        userId: userId,
-        userNickname: userNickname,
-        message: message,
-        time: "오늘",
-      })
-    );
-    console.log("send 2");
-    setMessage("");
-  };
+  // const sendMessage = (message) => {
+  //   console.log("send 1");
+  //   console.log(roomId);
+  //   let url = BASE_URL+"/app/chat/message";
+  //   console.log(url);
+  //   ws.send(
+  //     url,
+  //     {},
+  //     JSON.stringify({
+  //       // type: "TALK",
+  //       roomId: parseInt(roomId),
+  //       userId: userId,
+  //       userNickname: userNickname,
+  //       message: message,
+  //       time: "오늘",
+  //     })
+  //   );
+  //   console.log("send 2");
+  //   setMessage("");
+  // };
 
   const recvMessage = (recv) => {
     // setMessages(prevMessages => [
@@ -91,33 +158,34 @@ const Chat = () => {
     window.location.href = BASE_URL+"/chat/room";
   };
 
-  const connect = () => {
-    ws.connect(
-      {},
-      function(frame) {
-        ws.subscribe(BASE_URL+`/topic/chat/room/${roomId}`, (message) => {
-          var recv = JSON.parse(message.body);
-          recvMessage(recv);
-        })
-      },
-      function(error) {
-        if (reconnect++ <= 5) {
-          setTimeout(() => {
-            console.log('connection reconnect');
-            sock = new SockJS(BASE_URL+"/ws/chat");
-            ws = Stomp.over(sock);
-            // setReconnect(reconnect + 1);
-            connect();
-          }, 10 * 1000);
-        }
-      }
-    );
-  };
+  // const connect = () => {
+  //   ws.connect(
+  //     {},
+  //     function(frame) {
+  //       ws.subscribe(BASE_URL+`/topic/chat/room/${roomId}`, (message) => {
+  //         var recv = JSON.parse(message.body);
+  //         recvMessage(recv);
+  //       })
+  //     },
+  //     function(error) {
+  //       if (reconnect++ <= 5) {
+  //         setTimeout(() => {
+  //           console.log('connection reconnect');
+  //           sock = new SockJS(BASE_URL+"/ws/chat");
+  //           ws = Stomp.over(sock);
+  //           // setReconnect(reconnect + 1);
+  //           connect();
+  //         }, 10 * 1000);
+  //       }
+  //     }
+  //   );
+  // };
+  
 
   return (
     <div>
       <button onClick={() => {
-        connect(); 
+        // connect(); 
         initRoom();}}
       >chat</button>
       <div>
@@ -132,14 +200,18 @@ const Chat = () => {
           <label>내용</label>
         </div>
 
-        <form>
-          <input type='text' value={message} onChange={(e) => setMessage(e.target.value)} />
-          
-          {/* 전송 버튼 */}
-          <div>
-            <button type="button" onClick={() => sendMessage(message)}>보내기</button>
-          </div>
-        </form>
+        <input
+          type={"text"}
+          placeholder={"message"}
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          onKeyPress={(e) => e.which === 13 && publish(message)} 
+        />
+        
+        {/* 전송 버튼 */}
+        <div>
+          <button type="button" onClick={() => publish(message)}>보내기</button>
+        </div>
       </div>
 
       <div>
