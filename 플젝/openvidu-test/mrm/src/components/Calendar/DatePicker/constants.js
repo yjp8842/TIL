81e7b5d8705d955e8a45f5d5e4e6a95e2cef153f
yjp8842 <yjp20183029@gmail.com>
2023-02-17 22:21:30 +0900

    remove files

--- 플젝/openvidu-test/mrm/src/components/Calendar/DatePicker/constants.js
+++
@@ -1,8 +0,0 @@
-export const PERIOD = [
-    { id: "ALL", name: "전체" },
-    { id: "1W", name: "1주일" },
-    { id: "1M", name: "1개월" },
-    { id: "3M", name: "3개월" },
-    { id: "6M", name: "6개월" }
-  ];
-  
