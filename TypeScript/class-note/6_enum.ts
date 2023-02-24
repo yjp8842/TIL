031f760460aef36651fc9706dce706cbf6aecaf0
yjp8842 <yjp20183029@gmail.com>
2023-02-24 17:46:54 +0900

    TIL: typescript

---
+++ TypeScript/class-note/6_enum.ts
@@ -0,0 +1,17 @@
+// 숫자형
+// enum Shoes {
+//   Nike,
+//   Adidas
+// }
+
+// var myShoes = Shoes.Nike;
+// console.log(myShoes); // 0
+
+// 문자형
+enum Shoes {
+  Nike = '나이키',
+  Adidas = '아디다스'
+}
+
+var myShoes = Shoes.Nike;
+console.log(myShoes); // '나이키'
