a2714bbdb603314893947af227acd54bb283ee43
yjp8842 <yjp8842@naver.com>
2023-10-12 12:03:17 +0900

    TIL 231012 Java

---
+++ Java/java1/Programming/OkJavaGoInHome.java
@@ -0,0 +1,41 @@
+// 자동화의 열쇠 = 프로그래밍
+
+import javax.swing.JOptionPane;
+
+import org.opentutorials.iot.DimmingLights;
+import org.opentutorials.iot.Elevator;
+import org.opentutorials.iot.Lighting;
+import org.opentutorials.iot.Security;
+
+public class OkJavaGoInHome {
+
+	// args -> parameter, 매개변수
+	public static void main(String[] args) {
+		// Input
+//		String id = JOptionPane.showInputDialog("Enter a ID");
+//		String bright = JOptionPane.showInputDialog("Enter a Bright level");
+		
+		String id = args[0];
+		String bright = args[1];
+		
+		// Elevator call
+		Elevator myElevator = new Elevator(id);
+		myElevator.callForUp(1);
+	 
+		// Security off
+		Security mySecurity = new Security(id);
+		mySecurity.off();
+		
+		// Light on
+		Lighting hallLamp = new Lighting(id+" / Hall Lamp");
+		hallLamp.on();
+		
+		Lighting floorLamp = new Lighting(id+" / Floor Lamp");
+		floorLamp.on();
+		
+		DimmingLights moodLamp = new DimmingLights(id+" moodLamp");
+		moodLamp.setBright(Double.parseDouble(bright));
+		moodLamp.on();
+	}
+
+}
