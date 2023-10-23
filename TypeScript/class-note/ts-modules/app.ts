4d1244eee6425f6a59dc2d77ce464f90946321b5
yjp8842 <yjp8842@naver.com>
2023-10-23 23:58:29 +0900

    delete file

--- TypeScript/class-note/ts-modules/app.ts
+++
@@ -1,6 +0,0 @@
-import { Todo } from './types';
-
-var item: Todo = {
-  title: '할 일 1',
-  checked: false,
-}
--- TypeScript/getting-started/index.ts
+++
@@ -1,5 +0,0 @@
-function sum(a: number, b: number): number {
-  return a + b;
-}
-
-sum(10, 20);
